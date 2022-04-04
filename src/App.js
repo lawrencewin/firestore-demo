19b39de24f002165721daea9f9385af3b71ed205
Larry Win <lawrence.j.win@gmail.com>
2022-04-04 13:14:42 -0500

    No firebase yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Message(props) {
+    return (
+        <div className="message">
+            <div className="message__sender">{props.sender}</div>
+            <div className="message__date">{props.date}</div>
+            <p className="message__content">{props.content}</p>
+        </div>
+    )
 }
 
-export default App;
+function Messages(props) {
+    return (
+        <div className="messages">
+            { props.messages.map((message) => <Message 
+                sender={message.sender}
+                date={message.date}
+                content={message.content}
+            />) }
+        </div>
+    )
+}
+
+function Inputs(props) {
+    return (
+        <div className="inputs">
+            <div className="inputs__group">
+                <label htmlFor="nameInput">Name</label>
+                <input id="nameInput" type="text" />
+            </div>
+            <div className="inputs__group">
+                <label htmlFor="messageInput">Your Message</label>
+                <textarea id="messageInput"></textarea>
+            </div>
+            <button>Send Message</button>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div className="app">
+            <h1>AET Chat</h1>
+            <Messages messages={[
+                { sender: "Larry", date: "9/4/20", content: "Hello!" }
+            ]} />
+            <Inputs />
+        </div>
+    )
+}
+
+export default App
